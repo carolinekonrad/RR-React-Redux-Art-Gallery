7dca31505572401e5f1604f80f478f0086b0f939
Caroline Konrad <ckonrad@live.unc.edu>
2022-06-27 15:09:42 -0400

    Redux activity, adding in buttons to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import {useSelector, useDispatch, connect} from 'react-redux'
+import {fetchData, incrementId, decrementId, customId, clearData} from './features/dataSlice'
+
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId
+})
 
 function App() {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
+
+  const renderImg = () => {
+    if(data.apiData){
+      return <img style={{width: '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title}></img>
+    }else{
+      return <p>image here</p>
+    }
+  }
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [props.obectId, dispatch])
   
   return (
     <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+
+      <div>
+        <button onClick={() => dispatch(fetchData())}>Render Image</button>
+        <button onClick={() => {
+          dispatch(clearData())
+          dispatch(fetchData())
+        }}>Clear</button>
+        <button onClick={() => {
+            dispatch(incrementId())
+            dispatch(fetchData())
+          }}>Next</button>
+        <button onClick={() => {
+            dispatch(decrementId())
+            dispatch(fetchData())
+          }}>Back</button>
+      </div>
+      <input value={data.objectId} onChange={(e) => {dispatch(customId(Number(e.target.value)))}}></input>
+      <div>
+        {data.objectId}
+        {renderImg()}
+      </div>
     </div>
   );
 }
